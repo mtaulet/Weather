2199f9f3ed7336e62161cae64d73a00167bb7b07
Marta Taulet Sánchez <mtaulet@bu.edu>
2019-12-20 14:42:36 -0800

    Initial Commit - Storyboard UI

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Marta Taulet Sánchez on 12/19/19.
+//  Copyright © 2019 Marta Taulet Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
