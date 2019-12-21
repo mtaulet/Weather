081a838134c5b6943032b71cb939b38741a8c5c1
Marta Taulet Sánchez <mtaulet@bu.edu>
2019-12-20 19:41:22 -0800

    add location services

--- Weather/Controller/WeatherViewController.swift
+++ Weather/Controller/WeatherViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreLocation
+
 
 class WeatherViewController: UIViewController {
     
@@ -14,17 +16,25 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
-
-    var weatherManager = WeatherManager()
     
+    var weatherManager = WeatherManager()
+    let locationManager = CLLocationManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        locationManager.delegate = self
+
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+                
         searchTextField.delegate = self
         weatherManager.delegate = self
     }
+
+    }
     
-}
 
 // MARK: - UITextFieldDelegate
 
@@ -77,3 +87,25 @@ extension WeatherViewController: WeatherManagerDelegate {
 
 }
 
+// MARK: - CLLocationManagerDelegate
+
+extension WeatherViewController: CLLocationManagerDelegate {
+    
+    @IBAction func locationPressed(_ sender: UIButton) {
+        locationManager.requestLocation()
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let lat = location.coordinate.latitude
+            let lon = location.coordinate.longitude
+            weatherManager.fetchWeather(latitude: lat, longitude: lon)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+}
--- Weather/Model/WeatherManager.swift
+++ Weather/Model/WeatherManager.swift
@@ -28,6 +28,11 @@ struct WeatherManager {
         performRequest(with: urlString)
     }
     
+    func fetchWeather(latitude: Double, longitude: Double) {
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
+        performRequest(with: urlString)
+    }
+    
     func performRequest(with urlString: String) {
         // 1. Create url
         if let url = URL(string: urlString) {
